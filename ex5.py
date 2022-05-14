1c81816de48054026e65857706da72bd45afece7
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-05-14 21:50:19 +0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,6 +1,11 @@
-def get_letters():
-    """this function returns a list of all the letters between a-z and A-Z"""
-    return [chr(letter) for letter in range(ord('a'),ord('z'))]\
-      +[chr(letter) for letter in range(ord('A'),ord('Z'))]
+def get_letters() -> list:
+    """
+    this function create a list of all the letters between a-z and A-Z
+    @param : none
+    @return: list of all the letters
+    """
+    return [chr(letter) for letter in range(ord('a'), ord('z'))] \
+           + [chr(letter) for letter in range(ord('A'), ord('Z'))]
 
-get_letters()
+
+print(get_letters())
