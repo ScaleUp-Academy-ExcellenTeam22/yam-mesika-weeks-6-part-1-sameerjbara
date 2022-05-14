317bb515199d3bebd2d81ba40bbdc5abc0fc4a09
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-05-14 20:31:17 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,11 +1,14 @@
-def my_filter(function,iterable):
-    """ run on the iterable and execute the function , we add the element only if the retrun value of 
-    the function is not false or 0"""
-    filtered=[]
-    for element in iterable:
-        result=function(element)
-        if result!="false" and result!=0 :
-            filtered.append(element)
+from typing import Callable
+
+
+def my_filter(function: Callable[[iter], str or int], iterable: iter) -> list:
+    """
+    returns a list from those elements of iterable for which function returns true or 1
+    @param: function: The filtering function
+    @param: iterable: iterable object
+    @return: the filtered list.
+    """
+    filtered = [element for element in iterable if function(element) not in [0, "false"]]
     return filtered
 
 
