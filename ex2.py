98caf5b1ea57d3a280b0a8c45a6811d98c20288f
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-05-14 20:37:07 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,6 +1,8 @@
 def get_positive_numbers():
-    """ gets from the user list of numbers (as str) conver it to int and insert it to a list,
-    run on the list and keeps only positive numbers"""
-    numbers=[int(x) for x in input("enter a list of numbers : ").split(",")]
-    positive_numbers=[number for number in numbers if number>0]
+    """
+    the function create a list of positive numbers only from the user input
+    @return positive_numbers : list of the positive numbers
+    """
+    numbers = input("enter a list of numbers : ").split(",")
+    positive_numbers = [int(x) for x in numbers if int(x) > 0]
     return positive_numbers
