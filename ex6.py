de4efc4958daeb634708a342a449847b8c850dd8
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-05-14 21:57:44 +0300

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,9 +1,11 @@
 import re
 
-def count_words(text):
-    """this function gets a text, lower case and removes non-letter characters  for each word in the text,
-    create a dict that contains each word and the length of the word, 
-    return the dict"""
-    words = {word: len(word) for word in re.sub(r'\W+',' ',text).lower().split()}
-    return words
 
+def count_words(text: str) -> dict:
+    """
+    this function modify each word in the text and insert it to a dict a long side the length of the word
+    @param text: the given string
+    @return : dict of the  modified words ( lower cased )  and each word length
+    """
+    words = {word: len(word) for word in re.sub(r'\W+', ' ', text).lower().split()}
+    return words
