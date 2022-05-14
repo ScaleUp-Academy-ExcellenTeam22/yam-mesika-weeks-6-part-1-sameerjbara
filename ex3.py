7c1c34940ef2e071cd0c1ce764561959d86230e1
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-05-14 21:04:26 +0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,8 +1,14 @@
 import time
 
-def timer(f,*items):
-    """this function measures how long it took for the given function to complete a the task on the objects"""
+
+def timer(f: callable, *items: any) -> float:
+    """
+    this function measures how long it took for the given function to complete the task on the objects
+    @param f : the given function
+    @param items: the parameters passed to the function
+    @return : the time measured to complete the task
+    """
     start = time.time()
     f(items)
     end = time.time()
-    return (end - start)
+    return end - start
